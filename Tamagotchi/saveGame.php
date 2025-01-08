20ce76adbda1a124501fe167f97462e58b8fcc92
Shahriar Srabon <srabon361@gmail.com>
2025-01-08 07:01:26 +0600

    saveGame.php

--- Tamagotchi/saveGame.php
+++ Tamagotchi/saveGame.php
@@ -1,11 +1,10 @@
 <?php
 include 'db_config.php';
 
-// Enable error reporting
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-// Get post data
 $username = $_POST['username'] ?? '';
 $hunger = $_POST['hunger'] ?? 50;
 $happiness = $_POST['happiness'] ?? 50;
@@ -16,7 +15,6 @@ if (empty($username)) {
     exit;
 }
 
-// Update user's game state
 $sql = "UPDATE users SET hunger = ?, happiness = ?, energy = ? WHERE username = ?";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("iiis", $hunger, $happiness, $energy, $username);
